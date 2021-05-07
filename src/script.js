dd1011f021bd959944f3490f253617a7c2290dc5
Usman Bashir <me@usmanbashir.com>
2021-05-07 03:05:41 +0300

    Done with Assignment

---
+++ src/script.js
@@ -0,0 +1,13 @@
+// Make sure the JS file is conected with HTML file
+
+// Starting temperature
+const celsius = ...; // Add some code to get the user input
+
+// Conversion Code
+const celsiusToFahrenheit = ...;
+const celsiusToKelvin = ...;
+
+// Print the results to the browser console
+console.log(`Celsius: #{...}`);
+console.log(`Fahrenheit: #{...}`);
+console.log(`Kelvin: #{...}`);
